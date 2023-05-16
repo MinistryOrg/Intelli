845ecb6562c16af96a1261faa121f14548639a7c
root <pgeo82@gmail.com>
2023-05-16 13:20:27 +0300

    open the link when the user clicks

--- app/src/main/java/com/mom/intelli/ui/screens/NewsScreen.kt
+++ app/src/main/java/com/mom/intelli/ui/screens/NewsScreen.kt
@@ -1,6 +1,8 @@
 package com.mom.intelli.ui.screens
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -36,11 +38,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import com.mom.intelli.R
@@ -324,6 +328,7 @@ fun NewsMainScreen(
 
 @Composable
 fun MajorNewsItem(news : Results) {
+    val context = LocalContext.current
     Card(
         modifier = Modifier
             .padding(horizontal = 15.dp, vertical = 10.dp)
@@ -343,7 +348,11 @@ fun MajorNewsItem(news : Results) {
         Text(
             text = news.title,
             modifier = Modifier.
-                padding(horizontal = 10.dp, vertical = 10.dp),
+                padding(horizontal = 10.dp, vertical = 10.dp)
+                .clickable {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(news.link))
+                    context.startActivity(intent)
+                },
             color = NewsTitleClr,
             fontWeight = FontWeight.Bold,
             fontSize = 16.sp)
