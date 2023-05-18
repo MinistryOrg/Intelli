af963d23fa35016087f6c233f509393eee405acf
coccalis <kokkalisxristos@gmail.com>
2023-05-18 20:43:30 +0300

    Fixed error with nav graph

--- app/src/main/java/com/mom/intelli/util/nav_graph/SmartHomeNavGraph.kt
+++ app/src/main/java/com/mom/intelli/util/nav_graph/SmartHomeNavGraph.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.navigation
 import com.mom.intelli.ui.IntelliViewModel
 import com.mom.intelli.ui.screens.smart_home_screens.AddDeviceScreen
 import com.mom.intelli.util.ESHOP_GRAPH_ROUTE
+import com.mom.intelli.util.SMART_HOME_GRAPH_ROUTE
 import com.mom.intelli.util.Screen
 
 fun NavGraphBuilder.smarthomeNavGraph(
@@ -15,7 +16,7 @@ fun NavGraphBuilder.smarthomeNavGraph(
 ) {
     navigation(
         startDestination = Screen.Eshop.route,
-        route = ESHOP_GRAPH_ROUTE,
+        route = SMART_HOME_GRAPH_ROUTE,
     ) {
         composable(
             route = Screen.SmartHomeAddDevice.route
